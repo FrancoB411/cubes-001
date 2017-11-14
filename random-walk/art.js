c19a212c45679bbc6eb3dbb276d64026c7d9c0a7
Franco Barbeite <francob411@gmail.com>
2017-11-13 22:52:16 -0500

    add acceleration. Reset random walker on mousepress

--- random-walk/art.js
+++ random-walk/art.js
@@ -1,17 +1,21 @@
 var position;
-var magnitude = 5;
+var acc = 15;
+var vel = 1;
 
 function setup() {
   height = window.innerHeight;
   width = window.innerWidth;
   createCanvas(width, height);
-  position = createVector(width/2, height/2);
+  resetPosition();
 }
+
 
 function draw() {
   background(4, 58, 74);
-  var velocity = createVector(random(-magnitude, magnitude), random(-magnitude, magnitude));
-  position = position.add(velocity);
+  var velocity = createVector(random(-vel, vel), random(-vel, vel));
+  var acceleration = createVector(random(-acc, acc), random(-acc, acc));
+; 
+  position = position.add(velocity.add(acceleration));
   fill(204, 102, 0);
   stroke(0);
   ellipse(position.x, position.y, 50, 50);
@@ -21,5 +25,10 @@ function draw() {
 
 // reset board when mouse is pressed
 function mousePressed() {
+  resetPosition();
+}
+
+function resetPosition() {
+  position = createVector(width/2, height/2);
 }
 

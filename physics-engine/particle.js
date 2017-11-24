3db2d5044f8bdcccc14b1554dadeeaf376769fd9
Franco Barbeite <francob411@gmail.com>
2017-11-24 14:09:54 -0500

    apply a force to the particle

---
+++ physics-engine/particle.js
@@ -0,0 +1,33 @@
+
+// Newton's second law: F = M * A
+// where:
+// F: Force
+// M: Mass
+// A: Acceleration
+
+function Particle() {
+  this.position = createVector(width/2, height/2);
+  this.velocity = createVector(0, 0);
+  this.acceleration = createVector(0, 0);
+
+  this.display = function() {
+    noStroke();
+    ellipse(this.position.x, this.position.y, 50, 50);
+    this.position.add(this.velocity.add(this.acceleration));
+    this.acceleration = createVector(0,0);
+    return this;
+  }
+
+  this.update = function() {
+    var mousePosition = createVector(mouseX, mouseY);
+    return this;
+  }
+
+  this.resetPosition = function() {
+    this.position = createVector(width/2, height/2);
+  }
+
+  this.applyForce = function(force) {
+    this.acceleration.add(force);
+  }
+}
---
+++ physics-engine/physics-engine.js
@@ -0,0 +1,30 @@
+var position;
+var acc = 15;
+var vel = 0.5;
+var particle;
+var noiseSeed;
+
+function setup() {
+  height = window.innerHeight;
+  width = window.innerWidth;
+  createCanvas(width, height);
+  particle = new Particle();
+  noiseSeed = 0;
+}
+
+function draw() {
+  background(4, 58, 74);
+  var gravity = createVector(random(-1, 1), 0.01);
+  particle.applyForce(gravity);
+  particle.display();
+  particle.update();
+}
+
+function randomColor() { 
+  return noise(noiseSeed * random(0, 0.01)) * 255; 
+}
+
+function mousePressed() {
+  particle.resetPosition();
+}
+
